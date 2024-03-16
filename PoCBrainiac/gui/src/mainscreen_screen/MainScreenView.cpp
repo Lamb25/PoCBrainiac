1c583d238d7732682e4764075da75500d1b10155
Cesar Cordero <cordero25cesar@gmail.com>
2024-03-15 21:54:17 -0700

    Function to set speedo and taco values

--- PoCBrainiac/gui/src/mainscreen_screen/MainScreenView.cpp
+++ PoCBrainiac/gui/src/mainscreen_screen/MainScreenView.cpp
@@ -17,13 +17,19 @@ void MainScreenView::tearDownScreen()
 
 void MainScreenView::handleTickEvent()
 {
+    setSpeedoValue();
+    setTacoValue();
+}
+
+void MainScreenView::setSpeedoValue()
+{
     int max = 0;
     int min = 0;
     int nextValue = 0;
     int currentValue = crcProg_Speedometer.getValue();
 
     crcProg_Speedometer.getRange(min, max);
-
+    
     if (currentValue == min)
     {
         increase = true;
@@ -34,7 +40,7 @@ void MainScreenView::handleTickEvent()
         increase = false;
     }
 
-    
+
     if (increase)
     {
         nextValue = currentValue + 1;
@@ -47,4 +53,38 @@ void MainScreenView::handleTickEvent()
 
     crcProg_Speedometer.setValue(nextValue);
     crcProg_Speedometer.invalidate();
+}
+
+void MainScreenView::setTacoValue()
+{
+    int max = 0;
+    int min = 0;
+    int nextValue = 0;
+    int currentValue = crcProg_Tacometer.getValue();
+
+    crcProg_Tacometer.getRange(min, max);
+
+    if (currentValue == min)
+    {
+        increase = true;
+    }
+
+    else if (currentValue == max)
+    {
+        increase = false;
+    }
+
+
+    if (increase)
+    {
+        nextValue = currentValue + 1;
+    }
+
+    else
+    {
+        nextValue = currentValue - 1;
+    }
+
+    crcProg_Tacometer.setValue(nextValue);
+    crcProg_Tacometer.invalidate();
 }
